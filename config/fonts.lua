f6bc9b6c6e11321260bb9c9ee24f180c5c39a211
tecpatl <zldarkist@126.com>
2024-03-26 09:09:29 +0800

    font mac

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,13 +1,22 @@
 local wezterm = require('wezterm')
-local platform = require('utils.platform')
+local platform = require('utils.platform')()
 
-local font_size = platform().is_mac and 12 or 9
+local font_size = platform.is_mac and 12 or 9
+
+local font = {}
+if platform.is_mac then
+   font = {
+      'MesloLGS NF',
+   }
+elseif platform.is_linux then
+   font = {
+      'MesloLGM Nerd Font Mono',
+      'Noto Sans CJK HK',
+   }
+end
 
 return {
-  font = wezterm.font_with_fallback {
-    'MesloLGM Nerd Font Mono',
-    'Noto Sans CJK HK'
-  },
+   font = wezterm.font_with_fallback(font),
    font_size = font_size,
 
    --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
